b764cf2ccbd1a5683677364bd36d98ea415546b5
lilam-gimenez <lilam.gimenez@adevinta.com>
2025-02-16 22:24:00 +0100

    Initial commit

---
+++ src/main/kotlin/io/demo/mslibrary/MsLibraryApplication.kt
@@ -0,0 +1,11 @@
+package io.demo.mslibrary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsLibraryApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsLibraryApplication>(*args)
+}
---
+++ src/test/kotlin/io/demo/mslibrary/MsLibraryApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.demo.mslibrary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsLibraryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
