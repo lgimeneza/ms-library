a4b4061056396e6d2f3f20af38841934ab094e06
lilam-gimenez <lilam.gimenez@adevinta.com>
2025-02-16 22:39:47 +0100

    refactor: add initial project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
+    idea
     kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.spring") version "2.1.10"
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    application
 }
 
 group = "io.demo"
-version = "0.0.1-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
@@ -18,12 +19,19 @@ repositories {
     mavenCentral()
 }
 
+val integrationTest by sourceSets.creating
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    val integrationTestImplementation by configurations.getting
+    integrationTestImplementation(project)
+    integrationTestImplementation("io.rest-assured:spring-mock-mvc:5.5.0")
+    integrationTestImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 kotlin {
@@ -34,4 +42,25 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+val integrationTestTask = tasks.register<Test>("integrationTest") {
+    description = "Runs integration tests."
+    group = "verification"
+    useJUnitPlatform()
+
+    testClassesDirs = integrationTest.output.classesDirs
+    classpath = configurations[integrationTest.runtimeClasspathConfigurationName] + integrationTest.output
+
+    shouldRunAfter(tasks.test)
+}
+
+tasks.check {
+    dependsOn(integrationTestTask)
+}
+
+idea {
+    module {
+        testSources.from(sourceSets["integrationTest"].allJava.srcDirs)
+    }
 }
